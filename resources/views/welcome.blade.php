842480e97c168e3e2f43327167c0446968d3681a
Marko <mj.jeza@gmail.com>
2025-09-19 21:46:31 +0200

    mala promjena

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <h1>Dobrodošli na webstranicu ovu</h1>
+        <h1>Dobrodošli na webstranicu</h1>
 
-        <p>Nadamo se da će te uživati</p>
+        <p>Nadamo se da će te uživati, kao i jaaa</p>
     </body>
 </html>
