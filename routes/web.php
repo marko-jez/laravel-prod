4dbcf7e2ad93eab82b230897d6961db87c507bd7
Marko <mj.jeza@gmail.com>
2025-09-19 20:50:23 +0200

    Promjena cachea

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\KnjiznicaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::resource('knjige', KnjiznicaController::class);
+
+
