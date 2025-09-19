3bb13e88a59b14835eebc0b9dd6ee856febec5ab
Marko <mj.jeza@gmail.com>
2025-09-19 21:42:40 +0200

    Proba početne

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\KnjiznicaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
 
 Route::resource('knjige', KnjiznicaController::class);
 
